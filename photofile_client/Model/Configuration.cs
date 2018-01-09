ba12cf1ad6fa183d1a50100fd18654a6f76b7e6b
K <psoc5lp@gmail.com>
2018-01-09 15:37:48 +0900

    UIデザインを作成

---
+++ photofile_client/Model/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace photofile_client.Model {
+    class Configuration {
+        public string KeyStoneUrl { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PhotoDir { get; set; }
+        public string LocalContentsDir { get; set; }
+    }
+}
